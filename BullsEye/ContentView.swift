c191669e620012e9a5d6a67188e0d42af65cecd6
k0neer <38388047+k0neer@users.noreply.github.com>
2020-03-29 14:50:03 -0400

    Initial Commit

---
+++ BullsEye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BullsEye
+//
+//  Created by Kiryl Beliauski on 3/29/20.
+//  Copyright © 2020 Kiryl Beliauski. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
