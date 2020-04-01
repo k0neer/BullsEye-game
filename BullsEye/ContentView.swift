d52374ed602f95a72e8c5328d1d9d4b0756d3185
k0neer <bkotutby1112@icloud.com>
2020-03-31 20:37:43 -0400

    Added the completed draft interface and set up the slider.

--- BullsEye/ContentView.swift
+++ BullsEye/ContentView.swift
@@ -9,13 +9,84 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //properties
+    //==========
+    
+    //User interface views
+    @State var alertIsVisible: Bool = false
+    @State var sliderValue: Double = 50
+    
+    //User interface content and layout
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Spacer()
+            
+            //Target row
+            HStack {
+                Text("Pull the bulls eye as close as you can to:")
+                Text("100")
+            }
+            
+            Spacer()
+            
+            //Slider row
+            HStack {
+                Text("1")
+                Slider(value: self.$sliderValue, in: 1...100)
+                Text("100")
+            }
+            
+            Spacer()
+            
+            //button row
+            Button(action: {
+                print("Button pressed!")
+                self.alertIsVisible = true
+            }) {
+                Text("Hit me!")
+            }
+            .alert(isPresented: self.$alertIsVisible) {
+                Alert(title: Text("Hello there!"),
+                      message: Text("The slider's value is \(Int(self.sliderValue.rounded()))."),
+                    dismissButton: .default(Text("Awesome!")))
+            }
+            
+            Spacer()
+            
+            //score row
+            HStack {
+                Button(action: {}) {
+                    Text("Start Over")
+                }
+                Spacer()
+                Text("Score:")
+                Text("999999")
+                
+                Spacer()
+                Text("Round:")
+                Text("999")
+                
+                Spacer()
+                Button(action: {}) {
+                    Text("Info")
+                }
+            }
+            .padding(.bottom, 20)
+        }
     }
+    
+    //Methods
+    //=======
 }
 
+//preview
+//=======
+
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+#endif
