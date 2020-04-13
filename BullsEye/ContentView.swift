403abf1135082b17eb8f75af4dd3d77dd8cc4a33
kbeliauski <bkotutby1112@icloud.com>
2020-04-13 16:09:29 -0400

    created the about view and added more ViewModifiers

--- BullsEye/ContentView.swift
+++ BullsEye/ContentView.swift
@@ -14,7 +14,10 @@ struct ContentView: View {
     //properties
     //==========
     
-    //User interface views
+    //colors
+    let midnightBlue = Color(red: 0, green: 0.2, blue: 0.4)
+    
+    //game stats
     @State var alertIsVisible = false
     @State var sliderValue = 50.0
     @State var target = Int.random(in: 1...100)
@@ -30,91 +33,100 @@ struct ContentView: View {
     
     //User interface content and layout
     var body: some View {
-        
-        VStack {
-            Spacer()
-            
-            //Target row
-            HStack {
-                Text("Pull the bulls eye as close as you can to:")
-                    .font(Font.custom("Arial Rounded MT Bold", size: 18))
-                    .foregroundColor(.white)
-                    .shadow(color: .black, radius: 5, x: 2, y: 2)
-                Text("\(target)")
-                    .font(Font.custom("Arial Rounded MT Bold", size: 24))
-                    .foregroundColor(.yellow)
-                    .shadow(color: .black, radius: 5, x: 2, y: 2)
-            }
-            
-            Spacer()
-            
-            //Slider row
-            HStack {
-                Text("1")
-                Slider(value: $sliderValue, in: 1...100)
-                Text("100")
-            }
-            
-            Spacer()
-            
-            //button row
-            Button(action: {
-                print("Button pressed!")
-                self.alertIsVisible = true
-            }) {
-                Text("Hit me!")
-                    .font(Font.custom("Arial Rounded MT Bold", size: 18))
-                    .foregroundColor(.black)
-            }
-            .background(Image("Button"))
-            .shadow(color: .black, radius: 5, x: 2, y: 2)
-            .alert(isPresented: $alertIsVisible) {
-                Alert(title: Text(alertTitle()),
-                      message: Text(scoringMessage()),
-                      dismissButton: .default(Text("Awesome!")) {
-                        self.startNewRound()
-                    }
-                )
-            }
-            
-            Spacer()
-            
-            //score row
-            HStack {
+        NavigationView {
+            VStack {
+                Spacer()
+                
+                //Target row
+                HStack {
+                    Text("Pull the bulls eye as close as you can to:")
+                        .modifier(LabelStyle())
+                    Text("\(target)")
+                        .modifier(ValueStyle())
+                }
+                
+                Spacer()
+                
+                //Slider row
+                HStack {
+                    Text("1")
+                        .modifier(LabelStyle())
+                    Slider(value: $sliderValue, in: 1...100)
+                        .accentColor(.green)
+                    Text("100")
+                        .modifier(LabelStyle())
+                }
+                
+                Spacer()
+                
+                //button row
                 Button(action: {
-                    self.startNewGame()
+                    print("Button pressed!")
+                    self.alertIsVisible = true
                 }) {
-                    Text("Start Over")
+                    Text("Hit me!")
+                        .modifier(ButtonLargeTextStyle())
                 }
-                Spacer()
-                Text("Score:")
-                    .font(Font.custom("Arial Rounded MT Bold", size: 18))
-                    .foregroundColor(.white)
-                    .shadow(color: .black, radius: 5, x: 2, y: 2)
-                Text("\(score)")
-                    .font(Font.custom("Arial Rounded MT Bold", size: 24))
-                    .foregroundColor(.yellow)
-                    .shadow(color: .black, radius: 5, x: 2, y: 2)
-                Spacer()
-                Text("Round:")
-                    .font(Font.custom("Arial Rounded MT Bold", size: 18))
-                    .foregroundColor(.white)
-                    .shadow(color: .black, radius: 5, x: 2, y: 2)
-                Text("\(round)")
-                    .font(Font.custom("Arial Rounded MT Bold", size: 24))
-                    .foregroundColor(.yellow)
-                    .shadow(color: .black, radius: 5, x: 2, y: 2)
-                Spacer()
-                Button(action: {}) {
-                    Text("Info")
+                .background(Image("Button")
+                    .modifier(Shadow())
+                )
+                .alert(isPresented: $alertIsVisible) {
+                    Alert(title: Text(alertTitle()),
+                          message: Text(scoringMessage()),
+                          dismissButton: .default(Text("Awesome!")) {
+                            self.startNewRound()
+                        }
+                    )
                 }
+                
+                Spacer()
+                
+                //score row
+                HStack {
+                    Button(action: {
+                        self.startNewGame()
+                    }) {
+                        HStack {
+                            Image("StartOverIcon")
+                            Text("Start Over")
+                                .modifier(ButtonSmallTextStyle())
+                        }
+                        
+                    }
+                    .background(Image("Button")
+                        .modifier(Shadow())
+                    )
+                    Spacer()
+                    Text("Score:")
+                        .modifier(LabelStyle())
+                    Text("\(score)")
+                        .modifier(ValueStyle())
+                    Spacer()
+                    Text("Round:")
+                        .modifier(LabelStyle())
+                    Text("\(round)")
+                        .modifier(ValueStyle())
+                    Spacer()
+                    NavigationLink(destination: AboutView()) {
+                        HStack {
+                            Image("InfoIcon")
+                            Text("Info")
+                                .modifier(ButtonSmallTextStyle())
+                        }
+                    }
+                    .background(Image("Button")
+                        .modifier(Shadow())
+                    )
+                }
+                .accentColor(midnightBlue)
+                .padding(.bottom, 20)
             }
-            .padding(.bottom, 20)
+            .onAppear() {
+                self.startNewGame()
+            }
+            .background(Image("Background"))
         }
-        .onAppear() {
-            self.startNewGame()
-        }
-        .background(Image("Background"))
+        .navigationViewStyle(StackNavigationViewStyle())
     }
     
     //Methods
@@ -163,6 +175,50 @@ struct ContentView: View {
     func resetSliderAndTarget() {
         sliderValue = Double.random(in: 1...100)
         target = Int.random(in: 1...100)
+    }
+}
+
+//View Modifiers
+//==============
+
+struct LabelStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(Font.custom("Arial Rounded MT Bold", size: 18))
+            .foregroundColor(.white)
+            .modifier(Shadow())
+    }
+}
+
+struct ValueStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(Font.custom("Arial Rounded MT Bold", size: 24))
+            .foregroundColor(.yellow)
+            .modifier(Shadow())
+    }
+}
+
+struct Shadow: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .shadow(color: .black, radius: 5, x: 2, y: 2)
+    }
+}
+
+struct ButtonLargeTextStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(Font.custom("Arial Rounded MT Bold", size: 18))
+            .foregroundColor(.black)
+    }
+}
+
+struct ButtonSmallTextStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(Font.custom("Arial Rounded MT Bold", size: 12))
+            .foregroundColor(.black)
     }
 }
 
